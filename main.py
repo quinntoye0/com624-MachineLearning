c89d0a35760ea4ea070e44e2eaaa4ab05caf3b36
Quinn Toye <4toyeq57@solent.ac.uk>
2023-11-20 16:23:17 +0000

    reformatted methods

--- main.py
+++ main.py
@@ -7,15 +7,26 @@ from pandas_datareader import data as pdr
 import yfinance as yf
 # Graphing/Visualisation
 
-
-# tickers list
+### Nasdaq Data Download ###
+# ------------------------ #
+# retrieve and create tickers list
+print("Importing top 100 tickers...")
 with open("nasdaq_100_tickers.txt") as tickersFile:
     tickersList = [line.rstrip('\n') for line in tickersFile]
+print("Tickers imported ✓")
 
 # override yahoo finance api
 yf.pdr_override()
-# Import the data frame (df) from yahoo finance using the specified stock as the ticker symbol
+# import the data frame (df) from yahoo finance using the tickersList to grab all stocks
+print("\nDownloading Nasdaq data...")
 df = yf.download(tickers=tickersList,period='1y',interval='1d')
+print("Nasdaq data downloaded ✓")
+
+### Dimensionality Reduction using LDA ###
+# -------------------------------------- #
+print("\nReducing data size for each stock...")
+
+
 
 print(df)
 
